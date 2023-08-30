1a7ca3b71f844548737f8f7181f6d68e05dc744c
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-08-30 23:52:30 +0000

    feature: load data from JSON

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 import Nav from 'react-bootstrap/Nav';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react-improve'
 import OpenButton from './components/OpenButton/OpenButton';
 import { AiOutlineTool, AiFillProfile } from 'react-icons/ai'
@@ -14,30 +14,36 @@ import { TbAbc } from "react-icons/tb";
 import MenuButton from './components/MenuButtton/MenuButton';
 import ColorTable from './components/ColorTable/ColorTable';
 import ZIndexInput from './components/ZIndexInput/ZIndexInput';
+import { saveAs } from 'file-saver';
+import { json } from 'react-router-dom';
+
 // import { useMobileOrientation } from 'react-device-detect';
 function App() {
   // const { isLandscape } = useMobileOrientation()
 
   const { editor, onReady, selectedObjects } = useFabricJSEditor()
   const [play, setPlay] = useState(false);
+  const [name, setName] = useState("Nombre composicion");
   // const [color, setColor] = useState("#ffffff");
   const [toolbarOpen, setToolbarOpen] = useState(false);
   const [propertiesOpen, setPropertiesOpen] = useState(false);
   const openToolbar = () => { setToolbarOpen((toolbarOpen) => !toolbarOpen) }
   const openProperties = () => { setPropertiesOpen((propertiesOpen) => !propertiesOpen) }
-  const [selectTool, setSelectedTool] = useState("CIRCLE_WHITE")
+  const [selectTool, setSelectedTool] = useState("CIRCLE_WHITE");
+  const [jsonData, setJsonData] = useState(null);
+  const fileInputRef = useRef(null);
   // this effect adds a shorcut for delete key
   useDeleteSelectedOnKeyPress(editor)
   const changeColor = (color) => {
     editor?.setFillColor(color)
   }
 
-  // const bringForward = () => {
-  //   editor?.moveForward();
-  // }
-  // const bringBack = () => {
-  //   editor?.sendBack();
-  // }
+  const bringForward = () => {
+    editor?.moveForward();
+  }
+  const bringBack = () => {
+    editor?.sendBack();
+  }
 
   const changeSelectedTool = (text) => {
     if (selectTool !== text) {
@@ -78,6 +84,42 @@ function App() {
       }
     }
   }
+  const changeName = (e) => {
+    setName(e.target.value)
+  }
+  const downloadJSON = () => {
+    const jsonData = JSON.stringify(editor?.toJSON(), null, 2);
+    const blob = new Blob([jsonData], { type: 'application/json' });
+    if (name.length > 0) {
+      saveAs(blob, `${name}.json`);
+    } else {
+      saveAs(blob, 'composicion_1.json');
+
+    }
+  };
+  const openFileDialog = () => {
+    console.log("read -1")
+    fileInputRef.current.click();
+  };
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    console.log("read 0")
+    if (file) {
+      const reader = new FileReader();
+      console.log("read 1")
+      reader.onload = async (e) => {
+        console.log("read 2")
+        const content = e.target.result;
+        const parsedData = await JSON.parse(content);
+        setJsonData(parsedData)
+      };
+      reader.readAsText(file);
+    }
+  };
+  // console.log(jsonData)
+  useEffect(() => {
+    editor?.loadJSON(jsonData)
+  },[jsonData])
   return (
     <div className="App" >
       <Nav
@@ -88,9 +130,20 @@ function App() {
 
         <OpenButton Icon={AiOutlineTool} condition={toolbarOpen} changeFunction={openToolbar} altDescription="Abrir Barra de Herramientas" title="Cerrar Barra de Herramientas" />
         <div className='w-75 d-flex justify-content-start  align-items-center '>
-          <button className='bg-transparent text-white border-0 h5 me-3 my-1  ]' title="Crear nueva composicion">Nueva</button>
-          <button className='bg-transparent text-white border-0 h5 me-3 my-1  ]' title="Abrir una composicion">Abrir</button>
-          <button className='bg-transparent text-white border-0 h5 me-3 my-1  ]' title="Abrir una composicion">Descargar</button>
+          <input className=' file-button bg-transparent text-white border-0 h5 me-3 my-1' type="text" value={name} onChange={changeName}></input>
+          <button className='bg-transparent text-white border-0 h5 me-3 my-1' title="Crear nueva composicion">Nueva</button>
+          <label htmlFor="fileInput">
+            <button onClick={openFileDialog} className='bg-transparent text-white border-0 h5 me-3 my-1' title="Abrir una composicion">Abrir</button>
+            <input
+              type="file"
+              id="fileInput"
+              accept=".json"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+            />
+          </label>
+          <button onClick={downloadJSON} className='bg-transparent text-white border-0 h5 me-3 my-1' title="Descargar composicion">Descargar</button>
           <PlayButton changeFunction={setPlay} condition={play} title="Pausar Composicion" altDescription="Reproducir Composicion" />
         </div>
         <OpenButton Icon={AiFillProfile} condition={propertiesOpen} changeFunction={openProperties} altDescription="Abrir menú de propiedades" title="Cerrar menú de propiedades" />
@@ -148,13 +201,13 @@ function App() {
               <div className='bg-white rounded text-dark h-25 w-100 p-2 d-flex flex-column'>
                 <h5>Colores</h5>
                 <div className='h-100 w-100 p-2 d-flex flex-row  gap-3 '>
-                  <ColorTable changeColor={changeColor}/>
+                  <ColorTable changeColor={changeColor} />
                 </div>
               </div>
               <div className='bg-white rounded text-dark h-25 w-100 p-2 d-flex flex-column'>
                 <h5>Z-Index</h5>
                 <div className='h-100 w-100 p-2 d-flex flex-row  gap-3 '>
-                  <ZIndexInput/>
+                  <ZIndexInput bringBack={bringBack} bringForward={bringForward} />
                 </div>
               </div>
             </div>
--- src/components/ZIndexInput/ZIndexInput.jsx
+++ src/components/ZIndexInput/ZIndexInput.jsx
@@ -1,15 +1,15 @@
 import MenuButton from "../MenuButtton/MenuButton"
 import {AiFillCaretDown, AiFillCaretUp} from "react-icons/ai";
-const ZIndexInput = () => {
+const ZIndexInput = ({bringBack, bringForward}) => {
   return (
     <div className="d-flex flex-row gap-3 justify-content-center w-100">
-        <MenuButton selected={true}>
+        <MenuButton onClick={bringBack} selected={true}>
             <AiFillCaretDown size={40}/>
         </MenuButton>
         <div className="h1 text-dark d-flex flex-row justify-content-center " style={{minWidth: "100px"}}>
             0
         </div>
-        <MenuButton selected={true}>
+        <MenuButton onClick={bringForward} selected={true}>
             <AiFillCaretUp size={40}/>
         </MenuButton>
     </div>
